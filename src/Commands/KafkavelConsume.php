b0315df029db2c0e128478385383b47733fcaa4c
Tom Lindley <tom@tlkr.co.uk>
2021-12-13 01:11:02 +0000

    Added more debug information to kafkavel:consume

--- src/Commands/KafkavelConsume.php
+++ src/Commands/KafkavelConsume.php
@@ -3,8 +3,10 @@
 namespace OnSecurity\Kafkavel\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 use Junges\Kafka\Exceptions\KafkaConsumerException;
 use OnSecurity\Kafkavel\Resources\Consumers\ConsumeManager;
+use OnSecurity\Kafkavel\Resources\Consumers\ConsumerMessage;
 
 class KafkavelConsume extends Command
 {
@@ -13,7 +15,7 @@ class KafkavelConsume extends Command
      *
      * @var string
      */
-    protected $signature = 'kafkavel:consume';
+    protected $signature = 'kafkavel:consume {--D|debug}';
 
     /**
      * The console command description.
@@ -23,6 +25,7 @@ class KafkavelConsume extends Command
     protected $description = 'Runs the consume process for OnSecurity Kafkavel';
 
     private ?ConsumeManager $consumeManager = null;
+    private bool $debug = false;
 
     /**
      * Create a new command instance.
@@ -46,6 +49,12 @@ class KafkavelConsume extends Command
         pcntl_signal(SIGINT, fn() => $this->handleSigInt());
         pcntl_signal(SIGTERM, fn() => $this->handleSigInt());
 
+        $this->debug = $this->option('debug');
+
+        if ($this->debug) {
+            $this->info('Consume Manager Debug Mode Enabled - Watch your log output');
+        }
+
         $this->info('Consume Manager Starting');
 
         $this->startConsumer();
@@ -55,6 +64,9 @@ class KafkavelConsume extends Command
     {
         try {
             $this->consumeManager = new ConsumeManager;
+            if ($this->debug) {
+                $this->info('Subscribing to topics: ' . implode(', ', $this->consumeManager->getTopics()));
+            }
             $this->consumeManager->start();
         } catch (KafkaConsumerException $e) {
             $this->consumeManager = null;
@@ -64,12 +76,49 @@ class KafkavelConsume extends Command
         }
     }
 
+    private function createConsumeManager(): void
+    {
+        $this->consumeManager = new ConsumeManager;
+        if ($this->debug) {
+            $this->consumeManager->beforeHandle(function(ConsumerMessage $consumerMessage, array $handlerClasses) {
+                echo "Test\n";
+                Log::debug('----------');
+                Log::debug(sprintf(
+                    '%s -> %s:%s -> %s',
+                    $consumerMessage->getTopicName(),
+                    $consumerMessage->getSchema(),
+                    $consumerMessage->getSchemaVersion(),
+                    implode(', ', $handlerClasses)
+                ));
+                Log::debug(json_encode($consumerMessage->getBody()));
+
+                return true;
+            });
+
+            $this->consumeManager->afterHandle(function(ConsumerMessage $consumerMessage, array $handlerClasses, array $results) {
+                Log::debug(json_encode($results));
+            });
+        }
+    }
+
     private function handleSigInt()
     {
         $this->info('Consume Manager gracefully shutting down...');
         if ($this->consumeManager !== null) {
             $this->consumeManager->stop();
         }
+
+        $handledMessages = $this->consumeManager->getHandledMessages();
+        $ignoredMessages = $this->consumeManager->getIgnoredMessages();
+        $totalMessages = $handledMessages + $ignoredMessages;
+        $handledPercentage = $totalMessages === 0 ? 100 : round(($handledMessages / $totalMessages) * 100, 2);
+
+        $this->comment(sprintf('Handled %d Ignored %d Handled Percentage %d%%',
+            $handledMessages,
+            $ignoredMessages,
+            $handledPercentage
+        ));
+
         $this->info('Consume Manager Shutdown');
         exit(0);
     }
